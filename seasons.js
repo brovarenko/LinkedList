c0192bc3a27963d836d8607d403e84b0c6e02fde
brovarenko <brovwg@gmail.com>
2021-11-04 16:32:18 +0200

    first commit

---
+++ seasons.js
@@ -0,0 +1,11 @@
+
+let seasons = new LinkedList()
+seasons.printList()
+seasons.addToHead('summer')
+seasons.addToHead('spring')
+seasons.printList()
+seasons.addToTail('fall')
+seasons.addToTail('winter')
+seasons.printList()
+seasons.removeHead()
+seasons.printList()
